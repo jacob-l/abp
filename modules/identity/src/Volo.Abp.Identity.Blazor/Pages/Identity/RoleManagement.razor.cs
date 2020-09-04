a938068bae04ecfe82e3a0ee549a9b99bf8ff91d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2020-09-04 22:52:09 +0300

    Introduce AbpPageBase

--- modules/identity/src/Volo.Abp.Identity.Blazor/Pages/Identity/RoleManagement.razor.cs
+++ modules/identity/src/Volo.Abp.Identity.Blazor/Pages/Identity/RoleManagement.razor.cs
@@ -1,125 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Blazorise;
-using Blazorise.DataGrid;
-using Volo.Abp.ObjectExtending;
-using Volo.Abp.Application.Dtos;
+﻿using Volo.Abp.ObjectExtending;
 
 namespace Volo.Abp.Identity.Blazor.Pages.Identity
 {
-    public partial class RoleManagement
+    public partial class RoleManagement 
     {
-        private int _currentPage;
-        private string _currentSorting;
-        private int? _totalCount;
-        
-        private IReadOnlyList<IdentityRoleDto> _roles;
-        
-        private IdentityRoleCreateDto _newRole;
-        private Guid _editingRoleId;
-        private IdentityRoleUpdateDto _editingRole;
-        
-        private Modal _createModal;
-        private Modal _editModal;
+        protected override IIdentityRoleAppService AppService => RoleAppService;
 
-        public RoleManagement()
+        protected override void CreateUpdateInput(IdentityRoleDto entityDto)
         {
-            _newRole = new IdentityRoleCreateDto();
-            _editingRole = new IdentityRoleUpdateDto();
-        }
-
-        protected override async Task OnInitializedAsync()
-        {
-            await GetRolesAsync();
-        }
-
-        private async Task GetRolesAsync()
-        {
-            var result = await RoleAppService.GetListAsync(
-                new PagedAndSortedResultRequestDto
-                {
-                    SkipCount = _currentPage * LimitedResultRequestDto.DefaultMaxResultCount,
-                    MaxResultCount = LimitedResultRequestDto.DefaultMaxResultCount,
-                    Sorting = _currentSorting
-                });
-
-            _roles = result.Items;
-            _totalCount = (int?)result.TotalCount;
-        }
-
-        private async Task OnDataGridReadAsync(DataGridReadDataEventArgs<IdentityRoleDto> e)
-        {
-            _currentSorting = e.Columns
-                .Where(c => c.Direction != SortDirection.None)
-                .Select(c => c.Field + (c.Direction == SortDirection.Descending ? " DESC" : ""))
-                .JoinAsString(",");
-
-            _currentPage = e.Page - 1;
-            await GetRolesAsync();
-            StateHasChanged();
-        }
-
-        private void OpenCreateModal()
-        {
-            _newRole = new IdentityRoleCreateDto();
-            _createModal.Show();
-        }
-
-        private void CloseCreateModal()
-        {
-            _createModal.Hide();
-        }
-
-        private async Task OpenEditModalAsync(Guid id)
-        {
-            var role = await RoleAppService.GetAsync(id);
-
-            _editingRoleId = id;
-
-            //TODO: User AutoMapper!
             _editingRole = new IdentityRoleUpdateDto
             {
-                Name = role.Name,
-                ConcurrencyStamp = role.ConcurrencyStamp,
-                IsDefault = role.IsDefault,
-                IsPublic = role.IsPublic
+                Name = entityDto.Name,
+                ConcurrencyStamp = entityDto.ConcurrencyStamp,
+                IsDefault = entityDto.IsDefault,
+                IsPublic = entityDto.IsPublic
             };
 
-            role.MapExtraPropertiesTo(_editingRole);
-
-            _editModal.Show();
-        }
-
-        private void CloseEditModal()
-        {
-            _editModal.Hide();
-        }
-
-        private async Task CreateRoleAsync()
-        {
-            await RoleAppService.CreateAsync(_newRole);
-            await GetRolesAsync();
-            _createModal.Hide();
-        }
-
-        private async Task UpdateRoleAsync()
-        {
-            await RoleAppService.UpdateAsync(_editingRoleId, _editingRole);
-            await GetRolesAsync();
-            _editModal.Hide();
-        }
-
-        private async Task DeleteRoleAsync(IdentityRoleDto role)
-        {
-            if (!await UiMessageService.ConfirmAsync(L["RoleDeletionConfirmationMessage", role.Name]))
-            {
-                return;
-            }
-
-            await RoleAppService.DeleteAsync(role.Id);
-            await GetRolesAsync();
+            entityDto.MapExtraPropertiesTo(_editingRole);
         }
     }
 }
